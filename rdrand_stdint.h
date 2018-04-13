f4228d7eab24a28795432f8e04c987dffdb9ec40
David Johnston <dj@deadhat.com>
2018-04-13 21:24:59 +0100

    Fixing header

--- rdrand_stdint.h
+++ rdrand_stdint.h
@@ -1,13 +1,13 @@
 #include <stdint.h>
 
-extern int rdrand16_step(unsigned short int *);
-extern int rdseed16_step(unsigned short int *);
+extern int rdrand16_step(uint16_t *therand);
+extern int rdseed16_step(uint16_t *therand);
 
-extern int rdrand32_step(uint32_t *);
-extern int rdseed32_step(uint32_t *);
+extern int rdrand32_step(uint32_t *therand);
+extern int rdseed32_step(uint32_t *therand);
 
-extern int rdrand64_step(uint64_t *);
-extern int rdseed64_step(uint64_t *);
+extern int rdrand64_step(uint64_t *therand);
+extern int rdseed64_step(uint64_t *therand);
 
 extern int rdrand_get_uint32_retry(uint32_t retry_limit, uint32_t *dest);
 extern int rdseed_get_uint32_retry(uint32_t retry_limit, uint32_t *dest);
